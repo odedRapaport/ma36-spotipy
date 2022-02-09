9906c39b538c65a8235b95cd4fe395b2477ff5fa
odedRapaport <rapaportoded@gmail.com>
2022-02-09 14:12:08 +0200

    align code

--- spotipy/extract/extract_all_songs.py
+++ spotipy/extract/extract_all_songs.py
@@ -1,13 +1,10 @@
 from music.music_manager import MusicManager
 from extract.reader import JsonReader
-import json
 from os import listdir
 
 
 def extract_all_songs(music_manager: MusicManager):
-    songs_path = ""
-    with open("configuration.json") as config_file:
-        songs_path = json.load(config_file).get('extract_music').get('path')
+    songs_path = JsonReader("configuration.json").read().get('extract_music').get('path')
     tracks_json_format = [JsonReader(songs_path+"\\"+file.title()).read() for file in listdir(songs_path)]
     for track in tracks_json_format:
         music_manager.add_track(track)
--- spotipy/main.py
+++ spotipy/main.py
@@ -1,4 +1,3 @@
-from extract.reader import JsonReader
 from music.music_manager import MusicManager
 from extract.extract_all_songs import extract_all_songs
 
