3adb84cd8c0fb0e776c93bec2aeb5635b31f0762
odedRapaport <rapaportoded@gmail.com>
2022-02-09 10:10:00 +0200

    add json reader

--- spotipy/extract/reader.py
+++ spotipy/extract/reader.py
@@ -1,7 +1,17 @@
 from abc import ABC, abstractmethod
+import json
 
 
 class Reader(ABC):
     @abstractmethod
     def read(self):
         pass
+
+
+class JsonReader(Reader):
+    def __init__(self, path: str):
+        self.path = path
+
+    def read(self):
+        with open(self.path) as json_file:
+            return json.load(json_file)
