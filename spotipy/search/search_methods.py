76d03115da7ddc270673b4ae4f614d4f8734aa6f
odedRapaport <rapaportoded@gmail.com>
2022-02-09 15:40:10 +0200

    add get_albums_by_artist method

--- spotipy/search/search_methods.py
+++ spotipy/search/search_methods.py
@@ -1,7 +1,11 @@
-from users.user import User
 from music.music_manager import MusicManager
 
 
 def get_all_artists(music_manager: MusicManager):
     return [artist.name for artist in music_manager.artists]
 
+
+def get_albums_by_artist(music_manager: MusicManager, artist_id: str):
+    for artist in music_manager.artists:
+        if artist.id == artist_id:
+            return [album.name for album in artist.albums]
