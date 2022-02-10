113aed6b3c4e32dd26c632c77156b95ad980755b
odedRapaport <rapaportoded@gmail.com>
2022-02-10 08:57:23 +0200

    add logs

--- spotipy/extract/extract_all_songs.py
+++ spotipy/extract/extract_all_songs.py
@@ -1,6 +1,7 @@
 from music.music_manager import MusicManager
 from extract.reader import JsonReader
 from os import listdir
+import logging
 
 
 def extract_all_songs(music_manager: MusicManager):
@@ -8,4 +9,7 @@ def extract_all_songs(music_manager: MusicManager):
     tracks_json_format = [JsonReader(songs_path+"\\"+file.title()).read() for file in listdir(songs_path)]
     for track in tracks_json_format:
         music_manager.add_track(track)
+    logging.basicConfig(filename=JsonReader("configuration.json").read().get('logging').get("logs_file_name"),
+                        format='%(asctime)s - %(levelname)s - %(message)s', filemode='a', level=logging.DEBUG)
+    logging.info("extract all songs")
     return music_manager
